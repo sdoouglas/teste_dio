dd3ff55a6a6a45b9f60d0f18418a65bbfb4e2d48
sdoouglas <douglas_1200@hotmail.com>
2021-11-15 16:54:41 -0300

    first commi

---
+++ Primeiroprograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+
+public class Primeiroprograma {
+	public static void main(String[] args) {
+		Livro livro = new Livro ();
+		Gato gato = new Gato ();
+	}
+}
+class Livro {
+	public String livro;
+	public Integer numPaginas;
+}
